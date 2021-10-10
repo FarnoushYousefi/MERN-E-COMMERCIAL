96e1b949cc8b214a8a1d346f2adefb2fec177b81
Farnoush <farnoush.yousefikejani@uwaterloo.ca>
2021-10-09 20:15:22 -0400

    fix the bug

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { LinkContainer } from 'react-router-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
@@ -7,25 +7,19 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <LinkContainer to="/">
-            <Navbar.Brand>ProShop</Navbar.Brand>
+          <LinkContainer to ='/'>
+          <Navbar.Brand>ProShop</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse
-            id="basic-navbar-nav"
-            style={{ justifyContent: 'flex-end' }}
-          >
+          <Navbar.Collapse id="basic-navbar-nav" style={{justifyContent:'flex-end'}}>
             <Nav className="ml-auto">
-              <LinkContainer>
-                <Nav.Link to="/cart">
-                  <i className="fas fa-shopping-cart"></i>Cart
-                </Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/login">
-                <Nav.Link>
-                  {' '}
-                  <i className="fas fa-user"></i>Sign In
-                </Nav.Link>
+            <LinkContainer to ='/cart'>
+              <Nav.Link>
+              <i className="fas fa-shopping-cart"></i>Cart
+              </Nav.Link>
+             </LinkContainer>
+             <LinkContainer to ='/login'>
+              <Nav.Link> <i className="fas fa-user"></i>Sign In</Nav.Link>
               </LinkContainer>
             </Nav>
           </Navbar.Collapse>

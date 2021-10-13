67522b519401a4650e24d71f7883541aeaf47e13
Farnoush <farnoush.yousefikejani@uwaterloo.ca>
2021-10-12 21:13:31 -0400

    add reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,15 @@
 import {createStore, combineReducers, applyMiddleware} from  'redux'
-
+import {productListReducer}from './reducers/productreducers'
 import thunk from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 
-const reducer=combinReducers({})
+const reducer=combineReducers({
+    //this is product list out of state
+    productList:productListReducer,
+})
 
 const initialState={}
 const middleware=[thunk]
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware([...middleware])))
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
+
+export default store
